56185ac607c26ddab2607b0f94f03cea76b18abf
myh <zyuhao7@yeah.net>
2025-01-13 19:39:55 +0800

    添加 mq_channeltest.cpp 测试文件，验证 ChannelManager 的通道打开功能

---
+++ Test/mq_channeltest.cpp
@@ -0,0 +1,12 @@
+#include "../server/channel.hpp"
+
+int main()
+{
+    mq::ChannelManager::ptr cmp = std::make_shared<mq::ChannelManager>();
+    cmp->openChannel("c1",
+                     std::make_shared<mq::VirtualHost>("host1", "./data/host1/message/", "./data/host1/host1.db"),
+                     std::make_shared<mq::ConsumerManager>(),
+                     mq::ProtobufCodecPtr(),
+                     muduo::net::TcpConnectionPtr(),
+                     threadpool::ptr());
+}
