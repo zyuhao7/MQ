3316aa1810f2a6b3f98977f36e509e5df7fb723c
myh <zyuhao7@yeah.net>
2025-01-13 20:33:34 +0800

    修改 mq_channeltest.cpp，替换 ChannelManager 为 ConnectionManager，更新通道连接功能的测试

--- Test/mq_channeltest.cpp
+++ Test/mq_channeltest.cpp
@@ -1,12 +1,12 @@
-#include "../server/channel.hpp"
+#include "../server/connection.hpp"
 
 int main()
 {
-    mq::ChannelManager::ptr cmp = std::make_shared<mq::ChannelManager>();
-    cmp->openChannel("c1",
-                     std::make_shared<mq::VirtualHost>("host1", "./data/host1/message/", "./data/host1/host1.db"),
-                     std::make_shared<mq::ConsumerManager>(),
-                     mq::ProtobufCodecPtr(),
-                     muduo::net::TcpConnectionPtr(),
-                     threadpool::ptr());
+    mq::ConnectionManager::ptr cmp = std::make_shared<mq::ConnectionManager>();
+    cmp->newConnection(std::make_shared<mq::VirtualHost>("host1", "./data/host1/message/", "./data/host1/host1.db"),
+                       std::make_shared<mq::ConsumerManager>(),
+                       mq::ProtobufCodecPtr(),
+                       muduo::net::TcpConnectionPtr(),
+                       threadpool::ptr());
+     
 }
